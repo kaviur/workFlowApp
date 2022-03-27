97f6e8f105ff05952b307d08c75fc5f72d129038
KarenUrbano <quemojojojo@hotmail.com>
2022-03-27 11:30:08 -0500

    layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { validate } from './features/user/userSlice';
 import Home from './pages/Home';
 import Login from './pages/Login';
-
 
 function App() {
   const dispatch = useDispatch()
@@ -12,14 +13,18 @@ function App() {
     dispatch(validate())
   },[])
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="*" element={<NotFound/>}/> */}
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-      </Routes>
-    </BrowserRouter>
+  return (   
+      <BrowserRouter>
+        <Navbar/>
+        <div className='max-w-screen-xl mx-auto'>
+          <Routes>
+            {/* <Route path="*" element={<NotFound/>}/> */}
+            <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter> 
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import NavItem from './NavItem';
+
+export default function Footer() {
+    return  (
+        <footer className="w-full px-5 pt-10 border-t-2 border-slate-300 dark:border-neutral-300 bg-neutral-100 dark:bg-neutral-800 text-stone-900 dark:text-stone-50">
+        <section className="flex flex-row pb-10 justify-evenly align-start">
+            <div className="flex flex-col p-2">
+            <p className="inline text-base">CONTACTO</p>
+            <nav>
+                <ul className="flex flex-col text-sm">
+                    <NavItem to="/" title={"contacto"}/>
+                    <NavItem to="/" title={"About us"}/>
+                    <NavItem to="/" title={"Another link"}/>
+                    <NavItem to="/" title={"Another link"}/>
+                    <NavItem to="/" title={"and other.."}/>
+                </ul>
+            </nav>
+            </div>
+            <div className="flex flex-col p-2">
+            <p className="text-base">SOBRE WORKFLOW.COM</p>
+            <nav>
+                <ul className="flex flex-col text-sm">
+                    <NavItem to="/" title={"contacto"}/>
+                    <NavItem to="/" title={"About us"}/>
+                    <NavItem to="/" title={"Another link"}/>
+                    <NavItem to="/" title={"Another link"}/>
+                    <NavItem to="/" title={"and other.."}/>
+                </ul>
+            </nav>
+            </div>
+            <div className="flex flex-col p-2">
+            <p className="text-base">REDES SOCIALES</p>
+            <nav>
+                <ul className="flex flex-col text-sm">
+                    <NavItem to="/" title={"contacto"}/>
+                    <NavItem to="/" title={"About us"}/>
+                    <NavItem to="/" title={"Another link"}/>
+                    <NavItem to="/" title={"Another link"}/>
+                    <NavItem to="/" title={"and other.."}/>
+                </ul>
+            </nav>
+            </div>
+        </section>
+        <section className="w-full p-2 border-t-2 border-slate-300 dark:border-neutral-300">
+            <h2 className='text-center'>
+            Copyright © YYYY-2022 workflow.com Dirección. Todos los derechos
+            reservados.
+            </h2>
+        </section>
+        </footer>
+    );
+}
